1f281110d8129823c01fa5286294b08b50bc94e5
Sena Kokoz Pazar <senakokoz@gmail.com>
2025-01-07 13:22:29 +0300

    Arrays in ts

---
+++ 02basics/myArray.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+//const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [1, 2],
+    [3, 4]
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({
+    name: "Sena",
+    isActive: false
+});
---
+++ 02basics/myArray.ts
@@ -0,0 +1,26 @@
+const superHeros: string[] = [];
+//const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string,
+  isActive: boolean
+}
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [
+  [1, 2],
+  [3, 4]
+]
+
+superHeros.push("spiderman");
+
+heroPower.push(2);
+
+allUsers.push({
+  name: "Sena",
+  isActive: false
+})
+
+export {}
